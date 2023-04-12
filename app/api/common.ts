a1f5786889685f51aa727279ba3bae5be0df4c65
honkinglin <a154571896@gmail.com>
2023-04-12 20:48:53 +0800

    feat: 修改接口

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,4 +1,4 @@
-export const API_DOMAIN = "http://30.20.203.33:8000";
+export const API_DOMAIN = "http://yike.pengyyy.cn";
 import { showToast } from "../components/ui-lib";
 
 export function commonFetch(url: string, options: Record<string, any> = {}) {
