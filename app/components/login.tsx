69ea557fd9146f911fafc0c5c838662f07a9903c
overtrue <anzhengchao@gmail.com>
2023-04-14 00:32:53 +0800

    fixup! fix: 登录框样式调整
    fix: 登录框样式调整

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -257,7 +257,7 @@ export function ActivateDialog() {
           onChange={(e) => setInviteCode(e.target.value)}
         />
         <button
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          className="text-white w-full border-blue-600 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           onClick={handleActivate}
         >
           激活
