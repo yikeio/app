5f8c2cbf762f8e1df3a5b6bd06fc25cc8c7994d0
honkinglin <a154571896@gmail.com>
2023-04-18 00:41:08 +0800

    feat: 重构项目结构

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     fontLoaders: [
       {
