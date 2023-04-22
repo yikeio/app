690caaa3a0aab114fce886e9ad5de98d1ec6d81d
overtrue <anzhengchao@gmail.com>
2023-04-22 22:55:11 +0800

    chore: classnames

--- components/menu-item.tsx
+++ components/menu-item.tsx
@@ -8,16 +8,19 @@ export default function MenuItem({ menu: { icon, label, href } }) {
   const Icon = icon
   href = href || "#"
   const { asPath: currentPath } = useRouter()
-  let classNames = `focus-visible:ring-ring ring-offset-background inline-flex h-9 w-full  hover:bg-secondary/80  items-center justify-start rounded-md px-3 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`
+  let baseClassNames = `focus-visible:ring-ring ring-offset-background inline-flex h-9 w-full  hover:bg-secondary/80  items-center justify-start rounded-md px-3 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`
+
+  let className = classNames(baseClassNames, "text-gray-600 hover:text-white")
 
   if (currentPath === href) {
-    classNames += "text-primray bg-secondary text-secondary-foreground"
-  } else {
-    classNames += "text-gray-600 hover:text-white "
+    className = classNames(
+      baseClassNames,
+      "text-primray bg-secondary text-secondary-foreground"
+    )
   }
 
   return (
-    <Link href={href} className={classNames}>
+    <Link href={href} className={className}>
       <Icon className="mr-2 h-4 w-4" />
       {label}
     </Link>
--- components/second-menubar.tsx
+++ components/second-menubar.tsx
@@ -1,10 +1,12 @@
+import classNames from "classnames"
+
 export default function SecondMenubar(props) {
   return (
     <div
-      className={
-        `fixed md:relative bg-white border-r inset-0 w-full lg:w-72 h-full shrink-0 z-10 p-6 flex flex-col gap-6 min-h-screen ` +
-          props.className || ""
-      }
+      className={classNames(
+        "fixed md:relative bg-white border-r inset-0 w-full lg:w-72 h-full shrink-0 z-10 p-6 flex flex-col gap-6 min-h-screen",
+        props.className || ""
+      )}
     >
       {props.children}
     </div>
