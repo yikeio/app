38f05c55f622f858c63743e9f133b290cb2dab93
overtrue <anzhengchao@gmail.com>
2023-04-22 22:58:43 +0800

    chore: classnames

--- components/menu-item.tsx
+++ components/menu-item.tsx
@@ -10,17 +10,16 @@ export default function MenuItem({ menu: { icon, label, href } }) {
   const { asPath: currentPath } = useRouter()
   let baseClassNames = `focus-visible:ring-ring ring-offset-background inline-flex h-9 w-full  hover:bg-secondary/80  items-center justify-start rounded-md px-3 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`
 
-  let className = classNames(baseClassNames, "text-gray-600 hover:text-white")
-
-  if (currentPath === href) {
-    className = classNames(
-      baseClassNames,
-      "text-primray bg-secondary text-secondary-foreground"
-    )
-  }
-
   return (
-    <Link href={href} className={className}>
+    <Link
+      href={href}
+      className={classNames(
+        baseClassNames,
+        currentPath === href
+          ? "text-primray bg-secondary text-secondary-foreground"
+          : "text-gray-600 hover:text-white"
+      )}
+    >
       <Icon className="mr-2 h-4 w-4" />
       {label}
     </Link>
