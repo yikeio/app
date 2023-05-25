cf1b9b6cace4b78c1d12e6189ea416f0153c0ce6
xixileng <896054682@qq.com>
2023-05-25 23:29:05 +0800

    feat: 禁止浏览器自动弹出翻译窗口

--- components/head.tsx
+++ components/head.tsx
@@ -12,6 +12,7 @@ export default function Head({ title = "" }) {
         name="viewport"
         content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
       />
+      <meta name="google" content="notranslate" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
