1f31d5461331639eabe34de5e27a185f6829619d
overtrue <anzhengchao@gmail.com>
2023-06-11 22:21:50 +0800

    feat: 主页

--- components/home/more-features.tsx
+++ components/home/more-features.tsx
@@ -1,19 +1,32 @@
-import { HeartIcon, KeyboardIcon, LanguagesIcon, RadioIcon, SpeakerIcon, TypeIcon } from "lucide-react"
+import {
+  HeartIcon,
+  KeyboardIcon,
+  LanguagesIcon,
+  MicIcon,
+  MonitorSmartphoneIcon,
+  Share2Icon,
+  TypeIcon,
+  Volume2Icon,
+} from "lucide-react"
 
 export default function HomeMoreFeatures() {
   const features = [
     { name: "语言翻译", description: "支持中英文互译，更多语言正在开发中。", icon: <LanguagesIcon /> },
     { name: "消息收藏", description: "您可以随时收藏喜欢的消息内容，以便于随时回顾。", icon: <HeartIcon /> },
-    { name: "内容阅读", description: "支持将回复内容以语音播报的形式播放。", icon: <SpeakerIcon /> },
-    { name: "语音输入", description: "不想打字，没关系，可以直接使用语音来完成输入。", icon: <RadioIcon /> },
+    { name: "内容阅读", description: "支持将回复内容以语音播报的形式播放。", icon: <Volume2Icon /> },
+    { name: "语音输入", description: "不想打字，没关系，可以直接使用语音来完成输入。", icon: <MicIcon /> },
     { name: "快捷键", description: "支持快捷键，可以更加方便快捷完成各种操作。", icon: <KeyboardIcon /> },
     { name: "字号调整", description: "支持字号调整，满足不同用户的需求。", icon: <TypeIcon /> },
     {
       name: "内容导出",
       description: "支持将消息内容导出为多种格式文件，方便您随时查阅和分享。",
-      icon: <LanguagesIcon />,
+      icon: <Share2Icon />,
     },
-    { name: "多端同步", description: "支持多端同步，随时随地享受 AI 带来的生产力提升。", icon: <LanguagesIcon /> },
+    {
+      name: "多端同步",
+      description: "支持多端同步，随时随地享受 AI 带来的生产力提升。",
+      icon: <MonitorSmartphoneIcon />,
+    },
   ]
   return (
     <div className="flex flex-col gap-20 rounded-xl ">
