42f6751784870e2f7dbf3eba037c866e1713e101
overtrue <anzhengchao@gmail.com>
2023-06-12 22:53:36 +0800

    chore: 导航默认到场景页

--- components/head/logo-button.tsx
+++ components/head/logo-button.tsx
@@ -5,7 +5,7 @@ export default function LogoButton() {
   return (
     <Link
       className="hidden h-full shrink-0 items-center justify-start gap-4 px-6 text-slate-700 hover:bg-primary-50 dark:text-slate-400 md:flex"
-      href="/"
+      href="/prompts"
     >
       <Image src="/logo.svg" height={24} width={24} alt="logo" />
       <div className="sr-only">一刻 AI</div>
