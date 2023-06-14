f3536cb93a3ab84ca8a4257fd866bfa91ada6896
overtrue <anzhengchao@gmail.com>
2023-06-14 17:23:49 +0800

    chore: 临时禁用 Google 登录

--- components/auth/oauth-buttons.tsx
+++ components/auth/oauth-buttons.tsx
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router"
 import { getAuthRedirectUrl } from "@/api/auth"
 import GitHubIcon from "@/icons/github.svg"
-import GoogleIcon from "@/icons/google.svg"
 
 import { Button } from "../ui/button"
 
@@ -23,14 +22,14 @@ export default function OAuthLoginButtons() {
         <GitHubIcon className="mr-2 h-5 w-5" /> <span>GitHub</span>
       </Button>
 
-      <Button
+      {/* <Button
         className="w-full text-foreground hover:bg-muted hover:text-accent-foreground"
         variant="outline"
         size="sm"
         onClick={() => handleRedirect("google")}
       >
         <GoogleIcon className="mr-2 h-5 w-5" /> <span>Google</span>
-      </Button>
+      </Button> */}
     </div>
   )
 }
